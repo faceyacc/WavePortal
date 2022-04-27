029dee36f5eaf4a0082a22f834d186c564af2383
TY <ty@TYs-MacBook-Air.local>
2022-04-27 00:12:05 -0700

    added script to deploy to Ethereum

---
+++ scripts/run.js
@@ -0,0 +1,29 @@
+// Compile
+// Deploy
+// Execute
+
+
+const main = async () => {
+
+    // Compile 
+    const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+    
+    // Deploy
+    const waveContract = await waveContractFactory.deploy(); // creates local Ethereum network
+    await waveContract.deployed();
+
+    // Execute
+    console.log("Contract deployed to:", waveContract.address);
+}
+
+const runMain = async () => {
+    try {
+        await main();
+        process.exit(0); // exit Node process without error
+    } catch (error) {
+        console.log(error);
+        process.exit(1); // exit Node process while indicating 'Uncaught Fatal Exception"
+    }
+};
+
+runMain();
