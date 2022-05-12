36f85d7dc0e1d2924e87d6a1260920aea8171bdc
faceyacc <justfacey@gmail.com>
2022-05-11 19:44:01 -0700

    Added frontend from replit

---
+++ WavePortal/src/ClearableInput.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  font-family: 'Arial';
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5px;
+  border: 1px solid;
+  border-radius: 5px;
+  &:hover {
+    box-shadow: 1px 1px 1px thistle;
+  }
+`;
+
+const TextInput = styled.input`
+  flex: 1 0;
+  min-width: 50px;
+  min-height: 25px;
+  font-size: inherit;
+  background-color: transparent;
+  padding-left: 5px;
+  border: 0;
+  &:focus {
+    outline: none;
+  }
+`;
+
+const Icon = styled.div`
+  flex: 0 0;
+`;
+
+export const ClearableInput = () => {
+  const [inputValue, setInputValue] = useState('');
+
+  return (
+    <Container>
+      <TextInput
+        value={inputValue}
+        onChange={(event) => {
+          setInputValue(event.target.value);
+        }}
+      />
+    </Container>
+  );
+};
